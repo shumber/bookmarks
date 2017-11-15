73f60bb87198e85fc7a5adafaa1f0940148afc0f
Scott Humber <scott@vitalitics.ca>
2017-11-14 20:35:54 -0330

    added delete route

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get('/api/bookmarks', (req, res) => {
     res.json({ bookmarks });
 })
 
+app.delete('/api/bookmarks/:index', (req, res) => {
+    if (req.params.index){
+        if (req.params.index > -1) {
+            console.log(req.params.index);
+            bookmarks.splice(req.params.index, 1);
+            return res.status(200).send();
+        }
+    }
+    res.status(400).send();
+})
+
 app.post('/api/bookmarks', upload.array(), (req, res) => {
     if (req.body && req.body.url) {
         bookmarks.push(req.body.url);
